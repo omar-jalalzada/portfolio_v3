c11eaea93cfc671ecc61d6b6e6bc6787f1bf8504
Omar Jalalzada <omar.jalalzada@gmail.com>
2014-08-18 09:36:14 -0700

    got fancy box working! woot

--- source/javascripts/main/base.js
+++ source/javascripts/main/base.js
@@ -37,6 +37,25 @@ $(document).ready(function() {
     }
   );
 
+
+  //
+  // $('.fancybox').fancybox();
+
+  $(".fancybox-thumb").fancybox({
+    // prevEffect  : 'none',
+    // nextEffect  : 'none',
+    helpers : {
+      title : {
+        type: 'outside'
+      },
+      thumbs  : {
+        width : 50,
+        height  : 50
+      }
+    }
+  });
+
+
 });
 
 
