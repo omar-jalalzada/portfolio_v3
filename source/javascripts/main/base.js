4113d2da04c80fd85df79fc13431cd85e045bdb1
Omar Jalalzada <ojtwisted@gmail.com>
2014-11-17 04:20:22 -0800

    tap icons, no mouse scroll js function, updating the left and right arrow

--- source/javascripts/main/base.js
+++ source/javascripts/main/base.js
@@ -1,43 +1,69 @@
 $(document).ready(function() {
-
+  
+    $('body').on({
+    'mousewheel': function(e) {
+        if (e.target.id == 'el') return;
+        e.preventDefault();
+        e.stopPropagation();
+        }
+    })
 
     $('#fullpage').fullpage({
       scrollingSpeed: 200,
       cc3: true,
       easing: 'easeInOutQuart',
       resize: false,
+      verticalCentered: true,
 
-      // menu: true,
       menu: '#main_bottom_nav',
       anchors: ['oj_bio','iOS_App_Macys', 'Adaptive_Site_SFMOMA', 'Andriod_App_AFV', 'Branding_Moovweb', 'Arc_Al_Jazeera_Hackathon'],
-      autoScrolling: true,
       loopHorizontal: false,
-      // continuousVertical: true,
+      continuousVertical: true,
       keyboardScrolling: true,
-      // paddingBottom: '10px',
-      // slidesNavigation: true,
+      
+      // autoScrolling: false,
+      // menu: true,
 
+      // slidesNavigation: true,
       // navigationTooltips: ['firstSlide', 'secondSlide'],
       // slidesNavigation: true,
       // slidesNavPosition: 'bottom',
-
       // slidesColor: ['#fff', '#eee', '#ddd', '#ccc', '#bbb'],
-      // verticalCentered: true,
-      // loopHorizontal: false,
-
-      // afterLoad: function(anchorLink, index){
+      
+      // afterLoad: function(anchorLink, index, slideIndex){
       //     //using index
-      //     if(index == '3'){
-      //         alert("Section 3 ended loading");
+      //     if(index == 2){
+      //       alert("Section 1, slide 5 ended loading");
       //     }
-      // }
+      // },
 
-      // afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex){
+      afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex){
+        if(index == 2 && slideIndex == 5){
+          // alert("hi");
 
-      //   //first slide of the second section
-      //   if(index == '1' && slideIndex == 2){
-      //     alert("hi");
-      //   }
+          // $(window).keypress(function( event ) {
+          //   if ( event.which == 39 ) {
+          //     // event.preventDefault();
+          //     // alert("oh hay boy" + index)
+          //   }
+          //   // xTriggered++;
+          //   // var msg = "Handler for .keypress() called " + xTriggered + " time(s).";
+          //   // $.print( msg, "html" );
+          //   // $.print( event );
+          // });
+          // $(window).keydown(function(e) {
+
+          //   key = e.keyCode
+          //   if (key == 39) {
+          //     alert("oh hay boy" + index)
+          //     $.fn.fullpage.moveTo(index + 1, 0);
+          //   }
+          // });
+        }
+        // if(index == 2 && slideIndex == 5){
+
+        // }
+      },
 
       //   //second slide of the second section (supposing #secondSlide is the
       //   //anchor for the second slide
@@ -45,7 +71,6 @@ $(document).ready(function() {
       //       alert("Second slide loaded");
       //   }
       // }
-
 
 
       //----------------------------------------
@@ -92,7 +117,7 @@ $(document).ready(function() {
     // nextEffect  : 'none',
     openSpeed: 100,
     closeSpeed: 10,
-    nextSpeed: 100,
+    nextSpeed: 200,
     scrollOutside: true,
     beforeLoad: function() {
       $.fn.fullpage.setKeyboardScrolling(false);
@@ -127,10 +152,10 @@ $(document).ready(function() {
       var c_url = $(this).attr("href")
       
       if(location.hash == c_url) {
-        $(this).addClass("active")
+        $(this).addClass("anchor_active")
         console.log(c_url)
       } else {
-        $(this).removeClass("active")
+        $(this).removeClass("anchor_active")
       }
     });
 
@@ -152,49 +177,49 @@ $(document).ready(function() {
 
 // $("#main_bottom_nav .slide_link_containers a").eac
 
-// //
+//
 // arrow key guide
 //
-// if(!readCookie("schmoney_in_my_pocket")) {
-//   console.log("stay based.")
-//   $(function() {
-//     $(".arrow_keys_container").addClass("show").delay(5000).queue(function(next){
-//         // $(this).addClass("hide");
-//         $(this).delay(200).queue(function(next){
-//           // $(this).remove();
-//         });
-//         next();
-//     });
-//     // createCookie("schmoney_in_my_pocket","cause_im_lit", 1);
-//   });
-// }
+if(!readCookie("schmoney_in_my_pocket")) {
+  console.log("stay based.")
+  $(function() {
+    $(".arrow_keys_container").addClass("show").delay(5000).queue(function(next){
+        // $(this).addClass("hide");
+        $(this).delay(200).queue(function(next){
+          // $(this).remove();
+        });
+        next();
+    });
+    // createCookie("schmoney_in_my_pocket","cause_im_lit", 1);
+  });
+}
 
-// function do_it(arrow_div) {
-//   var selecta = ".arrow_keys_container " + arrow_div
-//   console.log(selecta)
-//   $(selecta).addClass("pressed")
-//   setTimeout(function(){
-//     $(selecta).removeClass("pressed")
-//   }, 200)
-// };
+function do_it(arrow_div) {
+  var selecta = ".arrow_keys_container " + arrow_div
+  console.log(selecta)
+  $(selecta).addClass("pressed")
+  setTimeout(function(){
+    $(selecta).removeClass("pressed")
+  }, 200)
+};
 
-// $(window).keydown(function(e) {
-//   key = e.keyCode
-//   switch(key) {
-//     case 38:
-//       do_it(".a_1")
-//     break;
-//     case 37:
-//       do_it(".a_2")
-//     break;
-//     case 39:
-//       do_it(".a_4")
-//     break;
-//     case 40:
-//       do_it(".a_3")
-//     break;
-//   }
-// });
+$(window).keydown(function(e) {
+  key = e.keyCode
+  switch(key) {
+    case 38:
+      do_it(".a_1")
+    break;
+    case 37:
+      do_it(".a_2")
+    break;
+    case 39:
+      do_it(".a_4")
+    break;
+    case 40:
+      do_it(".a_3")
+    break;
+  }
+});
 
 // // var xTriggered = 0;
 // $( ".arrow_keys_container" ).keydown(function( event ) {
